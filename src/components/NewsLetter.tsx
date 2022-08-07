306da2a07024e9c481993bc3b3e8a21f597c31e6
Abdelwahab <ab28fb@gmail.com>
2022-08-07 18:54:02 +0100

    center

--- src/components/NewsLetter.tsx
+++ src/components/NewsLetter.tsx
@@ -1,34 +1,65 @@
 import styled from "styled-components";
+import {
+  AiOutlineFacebook,
+  AiOutlineInstagram,
+  AiOutlineTwitter,
+  AiOutlineYoutube,
+} from "react-icons/ai";
 
 const Container = styled.div`
   display: flex;
-  height: 50vh;
+  height: 49vh;
   margin-top: -4px;
   background-color: black;
   color: white;
   border-top: 1px solid #2c2c2d;
+  border-bottom: 1px solid #2c2c2d;
+  position: relative;
 `;
 
 const Left = styled.div`
-  flex: 1;
-
-  border-right: 1px solid #2c2c2d;
+  width: 25%;
 `;
 
 const Center = styled.div`
-  flex: 1;
-  border-right: 1px solid #2c2c2d;
+  width: 55%;
+  background-color: #2c2c2d;
+  clip-path: polygon(0 0, 75% 0, 100% 100%, 25% 100%);
+`;
+
+const CenterContent = styled.div`
+  position: absolute;
+  top: 0%;
+  width: 54.8%;
+  height: 100%;
+  left: 25.1%;
   display: flex;
+  justify-content: flex-start;
+  /* align-items: center; */
+  clip-path: polygon(0 0, 75% 0, 100% 100%, 25% 100%);
+  background-color: black;
+  padding-left: 10%;
+  padding-top: 4%;
+  font-size: 15px;
+  line-height: 30px;
+  font-weight: 500;
 `;
 
 const Right = styled.div`
-  flex: 1;
-  border-right: 1px solid #2c2c2d;
+  padding-top: 3%;
 `;
 
 const Form = styled.div``;
 
-const Column = styled.div``;
+const Column = styled.div`
+  margin-right: 50px;
+  & a {
+    display: block;
+  }
+  & a:hover {
+    color: grey;
+  }
+`;
 
 const IconsContainer = styled.div``;
 
@@ -42,7 +73,8 @@ const NewsLetter = () => {
           <button>{">"}</button>
         </Form>
       </Left>
-      <Center>
+      <Center></Center>
+      <CenterContent>
         <Column>
           <a href="#">Help Center</a>
           <a href="#">Warranty</a>
@@ -64,10 +96,15 @@ const NewsLetter = () => {
           <a href="#">Born in PC</a>
           <a href="#">Careers</a>
         </Column>
-      </Center>
+      </CenterContent>
       <Right>
         <h3>Follow Us</h3>
-        <IconsContainer></IconsContainer>
+        <IconsContainer>
+          <AiOutlineInstagram />
+          <AiOutlineYoutube />
+          <AiOutlineFacebook />
+          <AiOutlineTwitter />
+        </IconsContainer>
       </Right>
     </Container>
   );
