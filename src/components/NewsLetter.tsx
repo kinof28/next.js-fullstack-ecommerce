825a803d4d89dc981bc06e36bec26942fbc3d0cc
Abdelwahab <ab28fb@gmail.com>
2022-08-07 04:02:48 +0100

    Newsletter WIP

--- src/components/NewsLetter.tsx
+++ src/components/NewsLetter.tsx
@@ -2,24 +2,73 @@ import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
-  height: 10vh;
+  height: 50vh;
   margin-top: -4px;
   background-color: black;
   color: white;
+  border-top: 1px solid #2c2c2d;
 `;
 
-const Left = styled.div``;
+const Left = styled.div`
+  flex: 1;
 
-const Right = styled.div``;
+  border-right: 1px solid #2c2c2d;
+`;
 
-const Center = styled.div``;
+const Center = styled.div`
+  flex: 1;
+  border-right: 1px solid #2c2c2d;
+  display: flex;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  border-right: 1px solid #2c2c2d;
+`;
+
+const Form = styled.div``;
+
+const Column = styled.div``;
+
+const IconsContainer = styled.div``;
 
 const NewsLetter = () => {
   return (
     <Container>
-      <Left></Left>
-      <Right></Right>
-      <Center></Center>
+      <Left>
+        <h3>Hear it First</h3>
+        <Form>
+          <input />
+          <button>{">"}</button>
+        </Form>
+      </Left>
+      <Center>
+        <Column>
+          <a href="#">Help Center</a>
+          <a href="#">Warranty</a>
+          <a href="#">Contact Us</a>
+          <a href="#">Product Help</a>
+          <a href="#">Order status</a>
+          <a href="#">Recycling</a>
+          <a href="#">Pay Later with Klarna</a>
+        </Column>
+        <Column>
+          <a href="#">Product Selector</a>
+          <a href="#">Custom Product</a>
+          <a href="#">Bulk Orders</a>
+          <a href="#">Press Releases</a>
+          <a href="#">Sound Advice</a>
+        </Column>
+        <Column>
+          <a href="#">About</a>
+          <a href="#">Born in PC</a>
+          <a href="#">Careers</a>
+        </Column>
+      </Center>
+      <Right>
+        <h3>Follow Us</h3>
+        <IconsContainer></IconsContainer>
+      </Right>
     </Container>
   );
 };
