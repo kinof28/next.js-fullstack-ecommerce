c8c4e7c453a10df8d0665508473a4a2e3deb97eb
Abdelwahab <ab28fb@gmail.com>
2022-08-11 02:17:20 +0100

    WIP dropdown

--- src/components/UI/DropDown.tsx
+++ src/components/UI/DropDown.tsx
@@ -3,8 +3,11 @@ import styled from "styled-components";
 const Container = styled.div`
   background: white;
   color: black;
-  height: ${(props) => (props.displayed ? "15vh" : "0vh")};
+  height: ${(props) => (props.displayed ? "30vh" : "0vh")};
+  display: ${(props) => (props.displayed ? "flex" : "none")};
   width: 100%;
+  z-index: 1;
+  border-bottom: 1px solid #2c2c2d;
   position: absolute;
   transition: all 0.5s ease;
   & h3 {
@@ -13,10 +16,17 @@ const Container = styled.div`
 `;
 
 const Shop = styled.div``;
-const Inside = styled.div``;
+const Inside = styled.div`
+  display: flex;
+`;
 const Support = styled.div``;
 const Language = styled.div``;
-const ImageContainer = styled.div``;
+const ImageContainer = styled.div`
+  border-left: 1px solid #2c2c2d;
+  & img {
+    width: 100%;
+  }
+`;
 
 const DropDown = (props) => {
   return (
@@ -24,23 +34,23 @@ const DropDown = (props) => {
       <Shop></Shop>
       <Inside>
         <ImageContainer>
-          <img />
+          <img src="./dropdown/desktop-dropdown-1.jpg" />
           <h3>Music with a Mission</h3>
         </ImageContainer>
         <ImageContainer>
-          <img />
+          <img src="./dropdown/desktop-dropdown-2.jpg" />
           <h3>Find Your Frequency</h3>
         </ImageContainer>
         <ImageContainer>
-          <img />
+          <img src="./dropdown/desktop-dropdown-3.jpg" />
           <h3>Our Mission, Vision, and Values</h3>
         </ImageContainer>
         <ImageContainer>
-          <img />
+          <img src="./dropdown/desktop-dropdown-4.jpg" />
           <h3>Our Culture</h3>
         </ImageContainer>
         <ImageContainer>
-          <img />
+          <img src="./dropdown/desktop-dropdown-5.jpg" />
           <h3>Our Sustainable Efforts</h3>
         </ImageContainer>
       </Inside>
