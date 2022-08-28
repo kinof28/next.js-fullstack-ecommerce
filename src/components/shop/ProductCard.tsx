af80ee9485299437d0626894b12592f2eb8b6be7
Abdelwahab <ab28fb@gmail.com>
2022-08-28 19:21:33 +0100

    WIP product card

--- src/components/shop/ProductCard.tsx
+++ src/components/shop/ProductCard.tsx
@@ -1,4 +1,24 @@
+import styled from "styled-components";
+import { ProductPreview } from "../../features/products/productsSlice";
+
+const Container = styled.div``;
+const MainImage = styled.img``;
+const ModelImages = styled.div``;
+
 const ProductCard = (props) => {
-  return <h1>ProductCard</h1>;
+  const product: ProductPreview = props.product;
+  return (
+    <Container>
+      {/* <MainImage /> */}
+      <img src={product.mainImag} alt={product.title} />
+      <ModelImages>
+        {product.models.map((model) => (
+          <img src={model.image} alt={model.id.toString()} />
+        ))}
+      </ModelImages>
+      <p>{product.title}</p>
+      <p>{product.price}</p>
+    </Container>
+  );
 };
 export default ProductCard;
--- src/components/shop/Products.tsx
+++ src/components/shop/Products.tsx
@@ -1,11 +1,17 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+
 import { AppState } from "../../app/store";
 import {
   getProductsPreview,
   ProductPreview,
 } from "../../features/products/productsSlice";
 import ProductCard from "./ProductCard";
+
+const Container = styled.div`
+  display: flex;
+`;
 
 const Products = (props) => {
   const dispatch = useDispatch();
@@ -17,13 +23,15 @@ const Products = (props) => {
   }, []);
 
   return (
-    <div>
-      <h4>products: </h4>
-      {products.map((e) => (
-        <ProductCard product={e} />
-      ))}
+    <>
+      <Container>
+        <h4>products: </h4>
+        {products.map((e) => (
+          <ProductCard product={e} />
+        ))}
+      </Container>
       <button onClick={() => dispatch(getProductsPreview())}>Refresh</button>
-    </div>
+    </>
   );
 };
 export default Products;
