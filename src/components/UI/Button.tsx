d368cfc824989408eb5601073a1b0dd9afdc2798
Abdelwahab <ab28fb@gmail.com>
2022-08-30 08:24:31 +0100

    button WIP

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,8 +1,39 @@
 import styled from "styled-components";
 
-const ButtonElement = styled.button``;
+const ButtonElement = styled.div`
+  border: none;
+  cursor: pointer;
+  display: inline;
+  position: relative;
+  background-color: #ededed;
+  padding: 1rem 2rem;
+  font-size: 0.9rem;
+  overflow: hidden;
+  & div {
+    /* content: ""; */
+    width: 20px;
+    background-color: #ddd;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translate(-50%) skew(20deg);
+    transition: width 0.36s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  }
+  &:hover div {
+    width: 100%;
+  }
+  & span {
+    z-index: 10;
+  }
+`;
 
 const Button = (props) => {
-  return <ButtonElement>{props.value}</ButtonElement>;
+  return (
+    <ButtonElement>
+      <div></div>
+      <span>{props.value}</span>
+    </ButtonElement>
+  );
 };
 export default Button;
