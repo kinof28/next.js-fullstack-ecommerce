5f2c0d63b24161588a2219aa34ffa94f5c0d872c
Abdelwahab <ab28fb@gmail.com>
2022-08-31 19:34:01 +0100

    WIP inside skullcandy page

---
+++ src/components/inside-skullcandy/Section.tsx
@@ -0,0 +1,4 @@
+const Section = (props) => {
+  return <div>Section from inside skullcandy page</div>;
+};
+export default Section;
---
+++ src/components/inside-skullcandy/Slider.tsx
@@ -0,0 +1,4 @@
+const Slider = () => {
+  return <div>inside skullcandy Slider</div>;
+};
+export default Slider;
