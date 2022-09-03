f81d9773397fe10789ef2564a025775b80461ec3
Abdelwahab <ab28fb@gmail.com>
2022-09-03 23:06:27 +0100

    WIP support page UI

---
+++ src/components/support/Category.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+const Category = (props) => {
+  return (
+    <Container>
+      <p>Category</p>
+    </Container>
+  );
+};
+export default Category;
--- src/pages/support/index.tsx
+++ src/pages/support/index.tsx
@@ -1,7 +1,11 @@
+import styled from "styled-components";
 import { NextPage } from "next";
 import Head from "next/head";
 import Container from "../../components/UI/Container";
+import Category from "../../components/support/Category";
 
+const Search = styled.input``;
+const Categories = styled.div``;
 const SupportPage: NextPage = () => {
   return (
     <>
@@ -9,6 +13,13 @@ const SupportPage: NextPage = () => {
         <title>Support</title>
         <link rel="icon" href="/favicon-32x32.webp" />
       </Head>
+      <Search />
+      <Categories>
+        <Category
+          title="Product Help."
+          decription="Setup videos, user guides, and troubleshooting information."
+        />
+      </Categories>
       <Container>Support page</Container>
     </>
   );
