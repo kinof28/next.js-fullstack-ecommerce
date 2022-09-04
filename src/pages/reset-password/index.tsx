2fba30410188623b2a943fb0c0fd908622b91da3
Abdelwahab <ab28fb@gmail.com>
2022-09-04 04:46:55 +0100

    reset password page init

--- src/pages/reset-password/index.tsx
+++ src/pages/reset-password/index.tsx
@@ -1,10 +1,21 @@
+import Head from "next/head";
+import Announcement from "../../components/Announcement";
+import NewsLetter from "../../components/NewsLetter";
 import Container from "../../components/UI/Container";
+import Title from "../../components/UI/Title";
 
 const ResetPassword = () => {
   return (
-    <Container>
-      <h1>Reset Password Page</h1>
-    </Container>
+    <>
+      <Head>
+        <title>Reset Password</title>
+        <link rel="icon" href="/favicon-32x32.webp" />
+      </Head>
+      <Announcement />
+      <Title>Reset Password</Title>
+
+      <NewsLetter />
+    </>
   );
 };
 export default ResetPassword;
