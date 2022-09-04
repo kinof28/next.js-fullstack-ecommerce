2fe9356b2a3346e4d7803574fd58dfd28e5f039f
Abdelwahab <ab28fb@gmail.com>
2022-09-04 04:45:57 +0100

    login page init

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,22 @@
 import { NextPage } from "next";
+import Head from "next/head";
+import Announcement from "../../components/Announcement";
+import NewsLetter from "../../components/NewsLetter";
 import Container from "../../components/UI/Container";
+import Title from "../../components/UI/Title";
 
 const LoginPage: NextPage = () => {
   return (
-    <Container>
-      <h1>Login Page</h1>
-    </Container>
+    <>
+      <Head>
+        <title>Sign in</title>
+        <link rel="icon" href="/favicon-32x32.webp" />
+      </Head>
+      <Announcement />
+      <Title>Sign in</Title>
+
+      <NewsLetter />
+    </>
   );
 };
 export default LoginPage;
