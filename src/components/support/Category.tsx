289b309df35877526ba6d0465047e5abe4cb4d10
Abdelwahab <ab28fb@gmail.com>
2022-09-04 04:34:14 +0100

    support page UI

--- src/components/support/Category.tsx
+++ src/components/support/Category.tsx
@@ -1,10 +1,45 @@
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  flex-basis: 24%;
+  flex-grow: 1;
+  position: relative;
+  margin-top: 4rem;
+  & h3 {
+    cursor: pointer;
+    font-size: 2.2rem;
+    font-weight: 800;
+    text-transform: uppercase;
+    letter-spacing: -2px;
+    margin-bottom: 1rem;
+  }
+  & p {
+    cursor: pointer;
+    font-size: 1.1rem;
+    font-weight: 500;
+    margin-bottom: 5rem;
+    margin-right: 4rem;
+  }
+  & a {
+    position: absolute;
+    bottom: 0;
+    font-size: 0.8rem;
+    padding: 10px 2px;
+    text-transform: uppercase;
+    color: #969696;
+    border-bottom: 2px solid #fff;
+    transition: all 0.2s ease;
+  }
+  &:hover a {
+    border-bottom: 2px solid grey;
+  }
+`;
 const Category = (props) => {
   return (
     <Container>
-      <p>Category</p>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+      <a href="#">{props.title}</a>
     </Container>
   );
 };
--- src/pages/support/index.tsx
+++ src/pages/support/index.tsx
@@ -1,11 +1,48 @@
 import styled from "styled-components";
 import { NextPage } from "next";
 import Head from "next/head";
-import Container from "../../components/UI/Container";
 import Category from "../../components/support/Category";
+import NewsLetter from "../../components/NewsLetter";
+import { HiOutlineSearch } from "react-icons/hi";
 
-const Search = styled.input``;
-const Categories = styled.div``;
+const Container = styled.div`
+  background-color: black;
+  color: white;
+  min-height: 100vh;
+  padding: 4rem;
+`;
+const Search = styled.div`
+  position: relative;
+  & input {
+    width: 100%;
+    height: 10vh;
+    outline: none;
+    border: none;
+    margin: 5rem 0px;
+    padding: 0px 2rem 0px 5rem;
+    font-size: 1.1rem;
+    font-weight: 600;
+  }
+  & div#logo {
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 10vh;
+    width: 5rem;
+    margin: 5rem 0px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #000;
+    font-size: 2rem;
+  }
+`;
+const Categories = styled.div`
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+  margin-bottom: 8rem;
+`;
 const SupportPage: NextPage = () => {
   return (
     <>
@@ -13,14 +50,43 @@ const SupportPage: NextPage = () => {
         <title>Support</title>
         <link rel="icon" href="/favicon-32x32.webp" />
       </Head>
-      <Search />
-      <Categories>
-        <Category
-          title="Product Help."
-          decription="Setup videos, user guides, and troubleshooting information."
-        />
-      </Categories>
-      <Container>Support page</Container>
+      <Container>
+        <Search>
+          <input type="text" placeholder="Search the Skullcandy Help Center" />
+          <div id="logo">
+            <HiOutlineSearch />
+          </div>
+        </Search>
+        <Categories>
+          <Category
+            title="Product Help."
+            description="Setup videos, user guides, and troubleshooting information."
+          />
+          <Category
+            title="Order Status."
+            description="Order status and tracking."
+          />
+          <Category
+            title="Warranty."
+            description="how to submit a warranty claim."
+          />
+          <Category title="Returns." description="Returns and refund policy." />
+          <Category
+            title="Contact Skullcandy."
+            description="How to get in touch with custumer service in your region."
+          />
+          <Category
+            title="Ordering Information."
+            description="Billing, shipping, and ordering information"
+          />
+          <Category
+            title="Registration."
+            description="Register your skullcandy product."
+          />
+          <Category title="Legal." description="Legal information." />
+        </Categories>
+      </Container>
+      <NewsLetter />
     </>
   );
 };
