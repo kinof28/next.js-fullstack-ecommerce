6ec1e21552d6597a97f9d001adaac21fc1fec745
Abdelwahab <ab28fb@gmail.com>
2022-09-04 04:48:11 +0100

    subscribe page init

--- src/pages/subscribe/index.tsx
+++ src/pages/subscribe/index.tsx
@@ -1,11 +1,22 @@
 import { NextPage } from "next";
+import Head from "next/head";
+import Announcement from "../../components/Announcement";
+import NewsLetter from "../../components/NewsLetter";
 import Container from "../../components/UI/Container";
+import Title from "../../components/UI/Title";
 
 const SubscribePage: NextPage = () => {
   return (
-    <Container>
-      <h1>Subscribe Page</h1>
-    </Container>
+    <>
+      <Head>
+        <title>New Account</title>
+        <link rel="icon" href="/favicon-32x32.webp" />
+      </Head>
+      <Announcement />
+      <Title>New Account</Title>
+
+      <NewsLetter />
+    </>
   );
 };
 export default SubscribePage;
