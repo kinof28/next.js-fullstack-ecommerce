26544d1202795d19e3dd99061ff4137720906822
Abdelwahab <ab28fb@gmail.com>
2022-09-05 09:33:50 +0100

    subscribe page UI init

--- src/pages/subscribe/index.tsx
+++ src/pages/subscribe/index.tsx
@@ -3,6 +3,8 @@ import Head from "next/head";
 import styled from "styled-components";
 import Announcement from "../../components/Announcement";
 import NewsLetter from "../../components/NewsLetter";
+import Button from "../../components/UI/Button";
+import Checkbox from "../../components/UI/Checkbox";
 import Input from "../../components/UI/Input";
 import Title from "../../components/UI/Title";
 
@@ -10,9 +12,15 @@ const Container = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
+  margin-bottom: 1rem;
 `;
 const Wrapper = styled.div`
   width: 35%;
+  & div#button {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
 `;
 
 const SubscribePage: NextPage = () => {
@@ -32,6 +40,19 @@ const SubscribePage: NextPage = () => {
           <Input type="text" label="First name" required={true} />
           <Input type="text" label="Last name" required={true} />
           <Input type="text" label="Company Name" />
+          <Input type="text" label="Country" required={true} />
+          <Input type="text" label="Zip Code" required={true} />
+          <Input type="text" label="City" required={true} />
+          <Input type="text" label="State" required={true} />
+          <Input type="text" label="Address line 1" required={true} />
+          <Input type="text" label="Address line 2" />
+          <Input type="text" label="Phone number" required={true} />
+          <Checkbox />
+          <div id="button">
+            <div>
+              <Button value="Create account" dark={true} />
+            </div>
+          </div>
         </Wrapper>
       </Container>
       <NewsLetter />
