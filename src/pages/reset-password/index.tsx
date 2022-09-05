ca5602186a3bf79c58fae2d00092deeaa6b5bc2d
Abdelwahab <ab28fb@gmail.com>
2022-09-05 09:04:58 +0100

    reset password page ui

--- src/pages/reset-password/index.tsx
+++ src/pages/reset-password/index.tsx
@@ -1,9 +1,34 @@
 import Head from "next/head";
+import styled from "styled-components";
 import Announcement from "../../components/Announcement";
 import NewsLetter from "../../components/NewsLetter";
-import Container from "../../components/UI/Container";
+import Button from "../../components/UI/Button";
+import Input from "../../components/UI/Input";
 import Title from "../../components/UI/Title";
 
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+const Wrapper = styled.div`
+  width: 45%;
+  margin-bottom: 7rem;
+  & div#form-wrapper {
+    display: flex;
+    align-items: flex-end;
+    gap: 1rem;
+  }
+  & div#button {
+    height: 3rem;
+  }
+  & div#input {
+    width: 70%;
+  }
+  & p {
+    font-size: 0.9rem;
+  }
+`;
 const ResetPassword = () => {
   return (
     <>
@@ -13,7 +38,23 @@ const ResetPassword = () => {
       </Head>
       <Announcement />
       <Title>Reset Password</Title>
-
+      <Container>
+        <Wrapper>
+          <p>
+            Fill in your email below to request a new password. An email will be
+            sent to the address below containing a link to verify your email
+            address.
+          </p>
+          <div id="form-wrapper">
+            <div id="input">
+              <Input type="email" label="Email Address" />
+            </div>
+            <div id="button">
+              <Button value="Reset Password" dark={true} />
+            </div>
+          </div>
+        </Wrapper>
+      </Container>
       <NewsLetter />
     </>
   );
