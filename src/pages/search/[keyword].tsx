1a504570b89d1d20b44181dc0dab29a4af089ffa
Abdelwahab <ab28fb@gmail.com>
2022-09-06 18:33:16 +0100

    search page init

---
+++ src/pages/search/[keyword].tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+import Container from "../../components/UI/Container";
+
+const SearchPage: NextPage = () => {
+  return (
+    <Container>
+      <h1>Search page</h1>
+    </Container>
+  );
+};
+export default SearchPage;
