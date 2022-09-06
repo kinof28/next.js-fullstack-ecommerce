6a069a5f8445ae9f79db4ad6727ccdc72fc6a4ac
Abdelwahab <ab28fb@gmail.com>
2022-09-06 18:00:05 +0100

    card and search dropdown init

---
+++ src/components/UI/Card.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+const Card = () => {
+  return (
+    <Container>
+      <h1> Card </h1>
+    </Container>
+  );
+};
+export default Card;
---
+++ src/components/UI/Search.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+const Search = () => {
+  return (
+    <Container>
+      <h1> Search </h1>
+    </Container>
+  );
+};
+export default Search;
