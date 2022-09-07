17381d469f474fb6f66c1cb82b3f37405c4d9676
Abdelwahab <ab28fb@gmail.com>
2022-09-07 13:26:26 +0100

    cart page init

---
+++ src/pages/cart/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import Container from "../../components/UI/Container";
+import Title from "../../components/UI/Title";
+
+const CartPage: NextPage = () => {
+  return (
+    <Container>
+      <Title>Cart page</Title>
+    </Container>
+  );
+};
+export default CartPage;
